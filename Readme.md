afa4c6ba56ae345b5ee47d3b241460c49a50a41b
Crenom <crenom@mail.ru>
2021-10-30 18:46:15 +0300

    init

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Docker openvpn
+Для запуска:
+```
+$ chmod +x start.sh
+$ ./start.sh
+```
+
+
+
+#### Полезные ссылки
+https://github.com/dockovpn/docker-openvpn
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo HOST_ADDR=$(curl -s https://api.ipify.org) > .env && \
+docker-compose up -d && \
+docker-compose exec -d dockovpn wget -O /doc/Dockovpn/client.ovpn localhost:8080
